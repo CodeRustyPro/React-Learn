e416179e73cf6edf5206f0b42fbbef5877014b02
Dev Shah <devshah1032@gmail.com>
2021-04-16 11:50:35 +0530

    Added files NOT via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Header from './components/Header'
+import Landing from './components/Landing.jsx'
 function App() {
   return (
     <div>
-      <h1>I am Dev</h1>
-      <Header/>
+      <Landing/>
     </div>
   );
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+function Landing() {
+    return (
+        <React.Fragment>
+            <div className="heading-hero">
+                <h1>A <span>Dev</span> who does <br></br>  <span>Computers</span></h1>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Landing;
